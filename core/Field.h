79daef4a8f88b0cdac3b0c1bbde7f8e6207c39c6
Sivagiri Visakan <27341353+SivagiriVisakan@users.noreply.github.com>
2019-02-26 11:29:35 +0530

    Correct an incorrect Constructor name.

--- core/Field.h
+++ core/Field.h
@@ -5,7 +5,7 @@ class Field
     private:
         char name[20];
     public:
-        BaseField(char *name);
+        Field(char *name);
         /**
          * The method that will be called whenever a player lands on this particular field.
          * Has to be overriden from the sub-class.
