7a20dec1d2bca6bd504faf762e11d0196275142f
Sivagiri Visakan <siva.g.visakan@gmail.com>
2019-03-18 19:42:21 +0530

    core: Add upgradeCompany member function to Company.

--- core/Company.cpp
+++ core/Company.cpp
@@ -16,7 +16,7 @@ void Company::setCost(int c)
 
 void Company::calculateRent()
 {
-    if(level != NotUpgradable)
+    if(level != NotUpgradable) // TODO: This might be an unneccessary check. All companies are upgrade-able(utilities aren't)
     {
         rent = level * cost*0.9; // TODO:Check this for a better ratio
     }
@@ -27,3 +27,15 @@ void Company::calculateRent()
     
 }
 
+void Company::upgradeCompany()
+{
+    if(level < LevelThree)
+    {
+        level = FieldLevel(level + 1); // Upgrade the level by 1
+        calculateRent(); // Recalculate the rent for this level.
+    }
+    else
+    {
+        throw "Can't upgrade company"; // TODO: Check if this is needed or any better way.
+    }
+}
--- core/Company.h
+++ core/Company.h
@@ -8,6 +8,12 @@ public:
     Company(char *name, int cost);
     void setCost(int c);
     void calculateRent();
+
+    /**
+     * Expanding a company will upgrade it after checking if it can be upgraded and
+     * changes the currentLevel
+    */
+    void upgradeCompany();
 };
 
 #endif // COMPANY_H
