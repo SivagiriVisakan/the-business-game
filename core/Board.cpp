4c05965ed3b0616d5365241da44f18bb9d05bd59
Sivagiri Visakan <siva.g.visakan@gmail.com>
2019-04-16 20:04:01 +0530

    core: Re-order fields.

--- core/Board.cpp
+++ core/Board.cpp
@@ -5,36 +5,36 @@ Board::Board()
     //chance
     addUtility("Airport", 300);
     addUtility("Railway", 250);
-    addCompany("Amazon", 275,SOFTWARE);
     addCompany("Rolls Royce", 290, AUTOMOTIVES);
-    addCompany("H & M", 150 , FASHION);
     addCompany("Samsung", 270, ELECTRONICS);
     addCompany("Cadbury", 250, FOOD);
-    //prison
-    addCompany("Google",300,SOFTWARE);
-    //community chest
+    addCompany("H & M", 150 , FASHION);
+    addCompany("Amazon", 275,SOFTWARE);
     addCompany("Tesla", 175, AUTOMOTIVES);
-    addCompany("Facebook",150, SOFTWARE);
-    addCompany("Adidas",250, FASHION);
-    addCompany("Apple", 280, ELECTRONICS);
-    addUtility("Transports", 250);
-    addCompany("McDonalds",190, FOOD);
-    addCompany("KFC", 120, FOOD);
-    addCompany("Benz",250, AUTOMOTIVES);
-    addCompany("Nike",120, FASHION);
-    //water tax
-    addCompany("Xiaomi",250, ELECTRONICS);
     //community chest
-    addCompany("Audi", 300, AUTOMOTIVES);
-    addCompany("Intel", 300 , ELECTRONICS);
-    addCompany("Gucci", 280, FASHION);
-    addCompany("Domino's", 175, FOOD);
-    //chance
+    addCompany("Google",300,SOFTWARE);
+    //prison
+    addCompany("Apple", 280, ELECTRONICS);
+    addCompany("Adidas",250, FASHION);
+    addCompany("Facebook",150, SOFTWARE);
     addCompany("Microsoft", 250, SOFTWARE);
-    addUtility("Space station", 400);
-    addUtility("Stadium", 200);
+    addCompany("Domino's", 175, FOOD);
+    addCompany("Intel", 300 , ELECTRONICS);
+    //community chest
+    //water tax
+    addCompany("Benz",250, AUTOMOTIVES);
+    addCompany("McDonalds",190, FOOD);
+    addUtility("Transports", 250);
+    addCompany("KFC", 120, FOOD);
+    addCompany("Nike",120, FASHION);
+    addCompany("Xiaomi",250, ELECTRONICS);
+    addCompany("Audi", 300, AUTOMOTIVES);
+    addCompany("Gucci", 280, FASHION);
+    //chance
     addUtility("Power Plant", 190);
     //income tax
+    addUtility("Stadium", 200);
+    addUtility("Space station", 400);
 }
 
 void Board::addCompany(char* name, int cost, CompanyCategory category)
